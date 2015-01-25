2fbd38d1f6eeee214fcb36db8d168eea41915443
joe <joe@joeegan.co.uk>
2015-01-25 01:03:20 +0000

    ensure todays shows are filtered, not yesterdays

--- adapters/today.js
+++ adapters/today.js
@@ -2,7 +2,17 @@ var moment = require('moment');
 moment().format();
 
 var today = function(data) {
-	return data.schedule.DAY[1].time.map(function(item, idx, arr){
+	var day;
+	var dayStr;
+	var momentTime;
+	for (var d in data.schedule.DAY) {
+		dayStr = data.schedule.DAY[d].$.attr;
+		if (moment(dayStr, "YYYY-MM-DD").isSame(moment(), 'day')) {
+			day = data.schedule.DAY[d];
+			continue;
+		}
+	}
+	return day.time.map(function(item, idx, arr){
 		return {
 			time: item.$.attr,
 			show: item.show.map(function(item, idx, arr){
@@ -15,8 +25,9 @@ var today = function(data) {
 			})
 		}
 	}).filter(function(item, idx, arr) {
-		var momentTime = moment(item.time, "hh:mm a");
+		momentTime = moment(item.time, "hh:mm a");
 		return momentTime.isAfter(moment().subtract(1, 'hours'));
 	});
 }
+
 module.exports = today;
